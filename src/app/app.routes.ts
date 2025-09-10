afa4d4adfa832e3aaae652326195ee58d60a1120
JeronimoV <jeronimoevilar@outlook.com>
2025-09-10 11:28:14 -0300

    2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Inicio } from './pages/inicio/inicio';
 import { QuienSoy } from './pages/quien-soy/quien-soy';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Bienvenida } from './pages/bienvenida/bienvenida';
 
 export const routes: Routes = [
     {path:"inicio", component: Inicio},
     {path:"quien-soy", component: QuienSoy},
     {path:"login", component: Login},
     {path:"register", component: Register},
+    {path:"", component: Bienvenida},
     {path:"**", redirectTo:""}
 ];
--- src/app/pages/bienvenida/bienvenida.ts
+++ src/app/pages/bienvenida/bienvenida.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-bienvenida',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './bienvenida.html',
-  styleUrl: './bienvenida.css'
+  styleUrls: ['./bienvenida.css']
 })
 export class Bienvenida {
 
